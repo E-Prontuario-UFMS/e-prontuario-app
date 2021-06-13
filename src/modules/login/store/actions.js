8b0b1f1263025468495ed93ee9a7c20f185c2f62
ygorazambujabefore <ygor.azambuja@before.com.br>
2021-06-13 18:18:12 -0400

    retirado backend do login

--- src/modules/login/store/actions.js
+++ src/modules/login/store/actions.js
@@ -11,8 +11,6 @@ export const ActionSetGlobalUser = async ({ commit }, payload) => {
   });
 
   usuario.rga === "201608030164" ? (usuario.role = "PROFESSOR") : null;
-  const id = await criaOuBuscaEnfermeiro(usuario);
-  usuario.id = id;
   commit(types.SET_GLOBAL_USER, { token, usuario });
 };
 
@@ -20,15 +18,15 @@ export const ActionDeleteGlobalUser = ({ commit }) => {
   commit(types.DELETE_GLOBAL_USER);
 };
 
-const criaOuBuscaEnfermeiro = async usuario => {
-  console.log(usuario);
+// const criaOuBuscaEnfermeiro = async usuario => {
+//   console.log(usuario);
 
-  const content =
-    (await (await axios.get(`${BASE_URL}/academico`)).data?.content) ?? [];
-  console.log(content);
-  // const { data } = await axios.post(`${BASE_URL}/academico`, {
-  //   nome: usuario.nome,
-  //   rga: usuario.rga,
-  // });
-  // return data.id;
-};
+//   const content =
+//     (await (await axios.get(`${BASE_URL}/academico`)).data?.content) ?? [];
+//   console.log(content);
+//   // const { data } = await axios.post(`${BASE_URL}/academico`, {
+//   //   nome: usuario.nome,
+//   //   rga: usuario.rga,
+//   // });
+//   // return data.id;
+// };
