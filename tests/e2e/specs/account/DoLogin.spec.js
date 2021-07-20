b0bd92077d907a4729b41ba9a2333fea89ef2896
Ygor Azambuja <ygorazambuja@gmail.com>
2021-07-19 23:20:59 -0400

    mocks para api

--- tests/e2e/specs/account/DoLogin.spec.js
+++ tests/e2e/specs/account/DoLogin.spec.js
@@ -2,7 +2,7 @@ describe("Login e2e", () => {
   it("should not pass if user have a wrong password", () => {
     cy.visit("/");
     cy.get("[data-cy='email']").type("ygorazambuja@gmail.com");
-    cy.get("[data-cy='password']").type("eprontuario@2021");
+    cy.get("[data-cy='password']").type("Vemqda@2");
     cy.get('[data-cy="btn-next"]').click();
   });
 });
--- todos.js
+++ todos.js
@@ -1,6 +1,7 @@
 // TODO: Designar Documentos (criar nova tela redirecionando) [X]
 // TODO: Cadastro de Professores (ver com Balan)
-// TODO: Matriculas (CRUD)
+// TODO: Matriculas (CRUD) [deixar para depois]
 // TODO: Disciplinas (Card de Alunos) [X]
-// TODO: Reordenar layout de Preencher Documento
+// TODO: Reordenar layout de Preencher Documento [O]
 // TODO: Formulario de Sugestões [X]
+// TODO: Quando aluno, desabilitar as ações de Disciplina [X]
